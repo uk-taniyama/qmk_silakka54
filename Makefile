a4a2de22abcb5f0b9550c7cec009629dbf975b3f
uk-taniyama <uk.taniyama@gmail.com>
2026-01-26 01:33:23 +0900

    feat: add silakka54led sources

--- Makefile
+++ Makefile
@@ -1,16 +1,22 @@
+.PHONY: build
 build:
 	sh ./scripts/build.sh
 
+.PHONY: build-default
 build-default:
 	km=default sh ./scripts/build.sh
 
+.PHONY: build-mine
 build-mine:
 	km=mine sh ./scripts/build.sh
 
+.PHONY: clean
 clean:
 	sh ./scripts/clean.sh
 
+.PHONY: rebuild
 rebuild: clean build
 
+.PHONY: format
 format:
 	find keyboards -name "*.[ch]" | xargs clang-format -i
--- scripts/build.sh
+++ scripts/build.sh
@@ -3,21 +3,21 @@ cd ..
 
 set -e -x
 
-kb="${kb:-silakka54x}"
+kb="${kb:-silakka54led}"
 km="${km:-via}"
 
 git submodule sync --recursive
 git submodule update --init --recursive
 
-rsync -a --delete keyboards/silakka54/* qmk_firmware/keyboards/silakka54x
+rsync -a --delete keyboards/silakka54led/* qmk_firmware/keyboards/silakka54led
 
 cd qmk_firmware
 # patch docker_cmd.sh's docker image
 # sed -i.bak 's|ghcr.io/qmk/qmk_cli[^ ]* |ghcr.io/qmk/qmk_cli@sha256:2dc05fc9f32efebd6b05c2b8676ee548358bc7e151e9dbf4dac6b6eed4513b07 |g' util/docker_cmd.sh
 sh util/docker_build.sh "${kb}:${km}"
-mv silakka54x_*.??? ..
+mv silakka54led_*.??? ..
 
 cd ..
 echo "Generated firmware:"
-ls silakka54x_*.???
+ls silakka54led_*.???
 
