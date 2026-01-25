cf383543fd512e0bff9cf85188f2ca9f334a8a4a
uk-taniyama <uk.taniyama@gmail.com>
2026-01-26 01:53:21 +0900

    chore: apply recent changes to 'mine'

--- keyboards/silakka54led/keymaps/mine/keymap.c
+++ keyboards/silakka54led/keymaps/mine/keymap.c
@@ -3,64 +3,67 @@
 
 #include QMK_KEYBOARD_H
 
+#define IME_TOGG ALT(KC_GRV)
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
+    // default layer 0 : base
     [0] = LAYOUT(
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                                            KC_LGUI, MO(1),    KC_SPC,          KC_ENT,  KC_RCTL, KC_RALT
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                              KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                              KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    _______,
+        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                              KC_H,    KC_J,    KC_K,    KC_L,    _______, KC_ENT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                              KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                                            KC_LGUI, MO(2),   KC_SPC,          KC_SPC,  MO(3), KC_RALT
     ),
+    // default layer 1 : alt(jp)
     [1] = LAYOUT(
-        KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_PGUP, KC_PGDN, KC_HOME, KC_END,  KC_DEL,  KC_F12,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_LBRC, KC_RBRC,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                                             KC_TRNS, KC_TRNS, KC_TRNS,         KC_TRNS,  KC_TRNS, KC_TRNS
+        _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
+                                            _______, _______, _______,         _______, _______, _______
     ),
     [2] = LAYOUT(
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                                             KC_TRNS, KC_TRNS, KC_TRNS,         KC_TRNS,  KC_TRNS, KC_TRNS
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                             KC_F11,  KC_F12,  _______, _______, _______, _______,
+        DF_TOGG, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,                           KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
+        _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
+                                            _______, _______, _______,         _______, _______, _______
     ),
     [3] = LAYOUT(
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                                             KC_TRNS, KC_TRNS, KC_TRNS,         KC_TRNS,  KC_TRNS, KC_TRNS
+        _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
+                                            _______, _______, _______,         _______, _______, _______
     ),
     [4] = LAYOUT(
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                                             KC_TRNS, KC_TRNS, KC_TRNS,         KC_TRNS,  KC_TRNS, KC_TRNS
+        _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
+                                            _______, _______, _______,         _______, _______, _______
     ),
     [5] = LAYOUT(
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                                             KC_TRNS, KC_TRNS, KC_TRNS,         KC_TRNS,  KC_TRNS, KC_TRNS
+        _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
+                                            _______, _______, _______,         _______, _______, _______
     ),
     [6] = LAYOUT(
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                                             KC_TRNS, KC_TRNS, KC_TRNS,         KC_TRNS,  KC_TRNS, KC_TRNS
+        _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
+                                            _______, _______, _______,         _______, _______, _______
     ),
     [7] = LAYOUT(
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                                             KC_TRNS, KC_TRNS, KC_TRNS,         KC_TRNS,  KC_TRNS, KC_TRNS
+        _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
+                                            _______, _______, _______,         _______, _______, _______
     )
 };
 
@@ -73,40 +76,28 @@ const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
                        'L', 'L', 'L',  'R', 'R', 'R'
     );
 // clang-format on
-#ifdef RGBLIGHT_LAYERS
-#    define RGBLIGHT_LAYER_HSV(hsv) RGBLIGHT_LAYER_SEGMENT({0, 1, hsv})
-
-// clang-format off
-const rgblight_segment_t PROGMEM rgb_green[] = RGBLIGHT_LAYER_HSV(HSV_GREEN);
-
-const rgblight_segment_t PROGMEM rgb_red[] = RGBLIGHT_LAYER_HSV(HSV_RED);
-
-const rgblight_segment_t PROGMEM rgb_blue[] = RGBLIGHT_LAYER_HSV(HSV_BLUE);
-
-const rgblight_segment_t* const PROGMEM rgb_layers_master[] = RGBLIGHT_LAYERS_LIST(
-    rgb_blue,
-    rgb_red,
-    rgb_green);
-
-const rgblight_segment_t* const PROGMEM rgb_layers_slave[] = RGBLIGHT_LAYERS_LIST(
-    rgb_blue,
-    rgb_blue,
-    rgb_blue);
-// clang-format on
+#include "../../rgblight_layer_color.c"
+#include "../../translate_jp.c"
+#include "../../default_layer_toggle.c"
 
 void keyboard_post_init_user(void) {
-    if (is_keyboard_master()) {
-        rgblight_layers = rgb_layers_master;
-    } else {
-        rgblight_layers = rgb_layers_slave;
-    }
-}
-
-layer_state_t layer_state_set_user(layer_state_t state) {
-    rgblight_set_layer_state(0, layer_state_cmp(state, 0));
-    rgblight_set_layer_state(1, layer_state_cmp(state, 1));
-    rgblight_set_layer_state(2, layer_state_cmp(state, 2));
-    return state;
-}
-
+#ifdef CONSOLE_ENABLE
+    debug_enable = true;
+    eeconfig_init();
 #endif
+#ifdef RGBLIGHT_ENABLE
+    rgblight_apply_layer_color(layer_state | default_layer_state);
+#endif
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t* record) {
+    if (is_default_layer_alt()) {
+        if (!process_translate_jp(keycode, record)) {
+            return false;
+        }
+    }
+    if (!process_default_layer_toggle(keycode, record)) {
+        return false;
+    }
+    return true;
+}
