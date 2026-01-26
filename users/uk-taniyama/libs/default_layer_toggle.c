05ba78757f4b8ea588b594a8b0f991f9b7ee06d0
uk-taniyama <uk.taniyama@gmail.com>
2026-01-26 12:09:49 +0900

    fix: use default_layer_set for layer detection

--- users/uk-taniyama/libs/default_layer_toggle.c
+++ users/uk-taniyama/libs/default_layer_toggle.c
@@ -4,17 +4,17 @@
 #define LAYER_ALT 1
 
 bool is_default_layer_base(void) {
-    return IS_LAYER_ON(LAYER_BASE);
+    return layer_state_cmp(default_layer_state, LAYER_BASE);
 }
 
 bool is_default_layer_alt(void) {
-    return IS_LAYER_ON(LAYER_ALT);
+    return layer_state_cmp(default_layer_state, LAYER_ALT);
 }
 
 bool process_default_layer_toggle(uint16_t keycode, keyrecord_t* record) {
     if (keycode == DF_TOGG) {
         if (record->event.pressed) {
-            uint8_t next = is_default_layer_base() != LAYER_BASE ? LAYER_BASE : LAYER_ALT;
+            uint8_t next = is_default_layer_base() ? LAYER_ALT : LAYER_BASE;
             set_single_persistent_default_layer(next);
 #ifdef LIB_RGBLIGHT_LAYER_COLOR_ENABLE
             // apply color based only on new default layer (not momentary layers)
