37e11ab131dfb1c07929b6508f75646d2c406511
uk-taniyama <uk.taniyama@gmail.com>
2026-01-26 12:09:49 +0900

    keymap: changes

--- keyboards/silakka54led/keymaps/mine/config.h
+++ keyboards/silakka54led/keymaps/mine/config.h
@@ -3,3 +3,21 @@
 #pragma once
 
 #define DYNAMIC_KEYMAP_LAYER_COUNT 8
+
+#define TAPPING_TOGGLE 1
+
+// clang-format off
+#   define RGBLIGHT_LAYER_COLORS \
+    {HSV_BLUE},         \
+    {HSV_RED},          \
+    {HSV_GREEN},        \
+    {HSV_GREEN},        \
+    {HSV_ORANGE},       \
+    {HSV_CYAN},         \
+    {HSV_MAGENTA},      \
+    {HSV_SPRINGGREEN}
+// clang-format on
+// {HSV_PURPLE}
+// {HSV_YELLOW}
+// {HSV_CHARTREUSE}
+// {HSV_GOLD}
--- keyboards/silakka54led/keymaps/mine/keymap.c
+++ keyboards/silakka54led/keymaps/mine/keymap.c
@@ -10,7 +10,15 @@
 TAP_HOLD(GRV_ESC, KC_GRV, KC_ESC);
 TAP_HOLD_KEYS(&tap_hold_GRV_ESC);
 
-const layer_state_t jp_layer = (1U << 1);
+enum layer_number {
+    _US = 0,
+    _JP,
+    _US1,
+    _JP1,
+    _FN,
+};
+
+const layer_state_t jp_layer = (1U << _JP) | (1U << _JP1);
 
 #define KEY_OVERRIDE_JP(mods, key, jp_key) ko_make_with_layers(mods, key, jp_key, jp_layer)
 
@@ -19,41 +27,41 @@ const layer_state_t jp_layer = (1U << 1);
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // default layer 0 : base
-    [0] = LAYOUT(
+    [_US] = LAYOUT(
         GRV_ESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                              KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                              KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    _______,
-        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                              KC_H,    KC_J,    KC_K,    KC_L,    _______, KC_ENT,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                              KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
+        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                              KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                              KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                                            KC_LGUI, MO(2),   KC_SPC,          US_ZKHK, MO(3),   ALT_IME
+                                            KC_LALT, TT(_US1),KC_SPC,          KC_SPC,  TT(_US1),ALT_IME
     ),
     // default layer 1 : alt(jp)
-    [1] = LAYOUT(
-        _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
-                                            _______, _______, _______,         _______, _______, _______
+    [_JP] = LAYOUT(
+        GRV_ESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                              KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                              KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
+        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                              KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                              KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                                            KC_LALT, TT(_JP1),KC_SPC,          KC_SPC,  TT(_JP1),ALT_IME
     ),
-    [2] = LAYOUT(
-        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                             KC_F11,  KC_F12,  _______, _______, _______, _______,
-        DF_TOGG, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______,                           KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
-        _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
-                                            _______, _______, _______,         _______, _______, _______
+    [_US1] = LAYOUT(
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                             KC_F11,  KC_F12,  XXXXXXX, KC_MINS, KC_EQL,  KC_BSPC,
+        DF_TOGG, XXXXXXX, KC_UP,   XXXXXXX, XXXXXXX, XXXXXXX,                           XXXXXXX, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, KC_BSLS,
+        _______, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX,                           KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT, _______,
+        _______, C(KC_Z), C(KC_X), C(KC_C), C(KC_V), XXXXXXX,                           XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+                                            KC_LGUI, TG(_US1),TG(_FN),         TG(_FN), TG(_US1),KC_LGUI
     ),
-    [3] = LAYOUT(
-        _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
-                                            _______, _______, _______,         _______, _______, _______
+    [_JP1] = LAYOUT(
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                             KC_F11,  KC_F12,  XXXXXXX, KC_MINS, KC_EQL,  KC_BSPC,
+        DF_TOGG, XXXXXXX, KC_UP,   XXXXXXX, XXXXXXX, XXXXXXX,                           XXXXXXX, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, KC_BSLS,
+        _______, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX,                           KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT, _______,
+        _______, C(KC_Z), C(KC_X), C(KC_C), C(KC_V), XXXXXXX,                           XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+                                            KC_LGUI, TG(_JP1),TG(_FN),         TG(_FN), TG(_JP1),KC_LGUI
     ),
-    [4] = LAYOUT(
+    [_FN] = LAYOUT(
+        QK_BOOT, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, EE_CLR,
         _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
-                                            _______, _______, _______,         _______, _______, _______
+                                            _______, TG(_FN), TG(_FN),          TG(_FN), TG(_FN), _______
     ),
     [5] = LAYOUT(
         _______, _______, _______, _______, _______, _______,                           _______, _______, _______, _______, _______, _______,
