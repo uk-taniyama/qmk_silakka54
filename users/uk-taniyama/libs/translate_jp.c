43f32a49faff0d2319206fd738312ac548e6743f
uk-taniyama <uk.taniyama@gmail.com>
2026-01-26 12:09:49 +0900

    chore: TEMP add key repeat support to translate_jp

--- users/uk-taniyama/libs/translate_jp.c
+++ users/uk-taniyama/libs/translate_jp.c
@@ -1,60 +1,127 @@
 #include "libs.h"
 #include "keymap_japanese.h"
 
-void send_translate_code(int code) {
-    uint8_t saved_mods = get_mods();
+// NOTE register_code/unregister_code works better than tap_code16.
+
+#define MAX_TRANSLATED_KEYS 4
+
+typedef struct {
+    uint16_t original_keycode;
+    uint16_t registered_code;
+    bool     added_shift;
+    bool     removed_shift;
+} trans_state_t;
+
+static trans_state_t trans_states[MAX_TRANSLATED_KEYS] = {0};
+
+static trans_state_t* get_trans_state(uint16_t keycode) {
+    for (int i = 0; i < MAX_TRANSLATED_KEYS; i++) {
+        if (trans_states[i].original_keycode == keycode) {
+            return &trans_states[i];
+        }
+    }
+    return NULL;
+}
+
+void register_translated_code(uint16_t original, uint16_t code) {
+    // Find an empty slot
+    trans_state_t* state = get_trans_state(KC_NO);
+    if (state == NULL) {
+        return;
+    }
+
+    state->original_keycode = original;
+    state->added_shift      = false;
+    state->removed_shift    = false;
+    state->registered_code  = code & QK_BASIC_MAX;
+
     if (code & QK_LSFT) {
-        add_mods(MOD_MASK_SHIFT);
+        // Shift is required but not currently pressed -> Add Weak Shift
+        if (!(get_mods() & MOD_MASK_SHIFT)) {
+            add_weak_mods(MOD_BIT_LSHIFT);
+            state->added_shift = true;
+        }
     } else {
-        del_mods(MOD_MASK_SHIFT);
+        // Shift is not required but currently pressed -> Remove Real Shift
+        if (get_mods() & MOD_MASK_SHIFT) {
+            del_mods(MOD_BIT_LSHIFT);
+            state->removed_shift = true;
+        }
     }
-    register_code(code & QK_BASIC_MAX);
-    unregister_code(code & QK_BASIC_MAX);
-    set_mods(saved_mods);
+    register_code(state->registered_code);
 }
 
-void send_translate_jp_code(int normal_code, int shifted_code) {
-    uint8_t saved_mods = get_mods();
-    bool    has_shift  = saved_mods & MOD_MASK_SHIFT;
+void unregister_translated_code(uint16_t original) {
+    // Find the corresponding key
+    trans_state_t* state = get_trans_state(original);
+    if (state == NULL) {
+        return;
+    }
 
-    if (has_shift) {
-        send_translate_code(shifted_code);
+    unregister_code(state->registered_code);
+
+    if (state->added_shift) {
+        del_weak_mods(MOD_BIT_LSHIFT);
+    }
+    if (state->removed_shift) {
+        add_mods(MOD_BIT_LSHIFT);
+    }
+
+    state->original_keycode = KC_NO;
+}
+
+void send_translate_code(uint16_t original, uint16_t code, bool pressed) {
+    if (pressed) {
+        register_translated_code(original, code);
     } else {
-        send_translate_code(normal_code);
+        unregister_translated_code(original);
     }
 }
 
-void send_translate_jp_grv(int normal_code, int shifted_code) {
-    uint8_t saved_mods = get_mods();
-    bool    has_alt    = saved_mods & MOD_MASK_ALT;
-    if (has_alt) {
-        // alt + ` then send kc_grv
-        send_translate_jp_code(JP_ZKHK, JP_ZKHK);
+void send_translate_jp_code(uint16_t original, uint16_t normal_code, uint16_t shifted_code, bool pressed) {
+    // Determine based on current Mod state when pressed, search and release using original keycode when released
+    if (pressed) {
+        if (get_mods() & MOD_MASK_SHIFT) {
+            register_translated_code(original, shifted_code);
+        } else {
+            register_translated_code(original, normal_code);
+        }
     } else {
-        send_translate_jp_code(normal_code, shifted_code);
+        unregister_translated_code(original);
+    }
+}
+
+void send_translate_jp_grv(uint16_t original, uint16_t normal_code, uint16_t shifted_code, bool pressed) {
+    if (pressed) {
+        uint8_t mods = get_mods();
+        if (mods & MOD_MASK_ALT) {
+            register_translated_code(original, JP_ZKHK);
+        } else if (mods & MOD_MASK_SHIFT) {
+            register_translated_code(original, shifted_code);
+        } else {
+            register_translated_code(original, normal_code);
+        }
+    } else {
+        unregister_translated_code(original);
     }
 }
 
 void send_alt_grv(uint16_t keycode, keyrecord_t* record) {
     uint8_t saved_mods = get_mods();
-    add_mods(MOD_MASK_ALT);
+    set_mods(MOD_MASK_ALT);
     register_code(KC_GRV);
     unregister_code(KC_GRV);
     set_mods(saved_mods);
 }
 
 bool process_translate_jp(uint16_t keycode, keyrecord_t* record) {
-#define TRANS_GRV(code, jpCode, jpSCode)            \
-    case code:                                      \
-        if (!record->event.pressed) {               \
-            send_translate_jp_grv(jpCode, jpSCode); \
-        }                                           \
+#define TRANS_GRV(code, jpCode, jpSCode)                                     \
+    case code:                                                               \
+        send_translate_jp_grv(code, jpCode, jpSCode, record->event.pressed); \
         return false;
-#define TRANS(code, jpCode, jpSCode)                 \
-    case code:                                       \
-        if (!record->event.pressed) {                \
-            send_translate_jp_code(jpCode, jpSCode); \
-        }                                            \
+#define TRANS(code, jpCode, jpSCode)                                          \
+    case code:                                                                \
+        send_translate_jp_code(code, jpCode, jpSCode, record->event.pressed); \
         return false;
     switch (keycode) {
         TRANS_GRV(KC_GRV, JP_GRV, JP_TILD) // ` ~
